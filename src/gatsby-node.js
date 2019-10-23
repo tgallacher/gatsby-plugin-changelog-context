bcb55e4be00e50e2344079a1a502159f90d66f6e
Tom Gallacher <tgallacher@users.noreply.github.com>
2019-10-23 22:02:44 +0100

    feat: align date format to ISO standard + expose git options

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,26 +1,60 @@
 const git = require('simple-git/promise');
 
-const getChangelog = async path => {
-  const { all: changelog } = await git().log({ file: path });
+/**
+ * Retrieve the commit history for a given file.
+ *
+ * @param {string} path path to file to get log
+ * @param {object | undefined} opts Optional options to pass to the `git log` call
+ */
+const getChangelog = async (path, opts) => {
+  const gitOpts = Object.assign(
+    {},
+    {
+      'file': path,
+      '--date': 'iso8601-strict',
+      // Override default format so we can change the datetime format
+      // This is required as `simple-git` is pretty printing the log.
+      //
+      // We use the `%cd` pretty print config to make the datetime format adhere to the `--date` switch
+      'format': {
+        hash: '%H',
+        message: '%s',
+        refs: '%D',
+        body: '%b',
+        date: '%cd',
+        author_name: '%aN',
+        author_email: '%ae',
+      },
+    },
+    opts,
+  );
+
+  const { all: changelog } = await git().log(gitOpts);
 
   return changelog ? changelog : [];
 };
 
-exports.onCreatePage = async ({
-  page,
-  actions: { createPage, deletePage },
-  reporter,
-}) => {
+/**
+ *
+ * @param {object} helpers Gatsby Node API helpers
+ * @param {object} options Plugin options
+ * @param {object} options.git Options to pass to the git command
+ */
+exports.onCreatePage = async (
+  { page, actions: { createPage, deletePage }, reporter },
+  { git: gitOpts },
+) => {
   if (!page.context.fileAbsolutePath) {
-    reporter.info(
+    return reporter.info(
       `[gatsby-plugin-changelog-context]: No "fileAbsolutePath" context found for page: ${page.path}`,
     );
-
-    return; // do nothing
   }
 
   try {
-    const changelog = await getChangelog(page.context.fileAbsolutePath);
+    const changelog = await getChangelog(
+      page.context.fileAbsolutePath,
+      gitOpts,
+    );
 
     deletePage(page);
     createPage({
